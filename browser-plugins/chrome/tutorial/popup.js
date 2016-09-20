fd5a31e4e2e222890135e29ec5476f4dc56dbf7e
Adrian Bugfixer <adrian.bugfixer@gmail.com>
2016-09-20 08:51:55 +0200

    Dummy response handlers

--- browser-plugins/chrome/tutorial/popup.js
+++ browser-plugins/chrome/tutorial/popup.js
@@ -8,6 +8,8 @@
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
+WEBSITE_URL = "";
+
 function getCurrentTabUrl(callback) {
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
@@ -88,31 +90,112 @@ function getImageUrl(searchTerm, callback, errorCallback) {
   x.send();
 }
 
-function renderStatus(statusText) {
+function renderStatus(statusText, url) {
   document.getElementById('status').textContent = statusText;
+  document.getElementById('url').textContent = url;
+}
+
+function renderComments(comments) {
+  var commentsDiv = document.getElementById("comments");
+  commentsDiv.innerHTML = "";
+  for(var i = 0; i < comments.length; i++) {
+    var commentDiv = document.createElement("div");
+    commentDiv.setAttribute("id", "hv_comment_"+comments[i].id);
+    commentDiv.setAttribute("class", "comment");
+
+    var commentImg = document.createElement("img");
+    commentImg.setAttribute("src", "avatar_default.png");
+
+    commentDiv.appendChild(commentImg);
+
+    var commentHeader = document.createElement("div");
+    commentHeader.innerHTML += '<b>vonsuu</b><span class="timeAgo"> 4 hours ago</span><span style="float: right;">+ -</span>';
+
+    commentDiv.appendChild(commentHeader);
+
+    var commentContent = document.createTextNode(comments[i].content);
+
+    commentDiv.appendChild(commentContent);   
+
+    commentsDiv.appendChild(commentDiv);
+    //
+    //var newDiv = document.createElement("div");
+    //newDiv.setAttribute("id", "hv_comment_"+comments[i].id);
+    //var newContent = document.createTextNode(comments[i].content);   
+    //newDiv.appendChild(newContent);
+    //var commentsDiv = document.getElementById("comments"); 
+    //commentsDiv.appendChild(newDiv);
+  }
+}
+
+function initPlugin(url){
+    var api = "http://localhost:8080/api/website/";
+    var params = {
+      "uri": url
+    }
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", api, true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(params));
+    xmlhttp.onreadystatechange = function () { //Call a function when the state changes.
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          //alert(xmlhttp.responseText);
+          var response = JSON.parse(this.responseText);
+          WEBSITE_ID = response.id;
+          renderComments(response.comments);
+      }
+    }
+}
+ 
+function addComment(params) {
+  var xmlhttp = new XMLHttpRequest();
+  var api = "http://localhost:8080/api/website/"+WEBSITE_ID+"/comments";
+  xmlhttp.open("POST", api, true);
+  xmlhttp.setRequestHeader("Content-type", "application/json");
+  xmlhttp.send(JSON.stringify(params));
+  xmlhttp.onreadystatechange = function () { //Call a function when the state changes.
+    if (xmlhttp.readyState == 4) {
+      getComments(function(comments){
+        renderComments(comments);
+      });
+    }
+  }
+}
+
+function getComments(callback) {
+  var xmlhttp = new XMLHttpRequest();
+  var api = "http://localhost:8080/api/website/"+WEBSITE_ID+"/comments";  
+  var comments = {"id":0,"content":"blabla"};
+  xmlhttp.open("GET", api, true);
+  xmlhttp.setRequestHeader("Content-type", "application/json");
+  xmlhttp.send();
+  xmlhttp.onreadystatechange = function () { //Call a function when the state changes.
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      comments = JSON.parse(this.responseText);
+    }
+    callback(comments);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  var api = "http://localhost:8080/api/website/";
+  var addCommentSubmit = document.getElementById('comment_submit');
+
   getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+    WEBSITE_URL = url;
+    document.getElementById('url').textContent = url;
+    initPlugin(WEBSITE_URL);
+  });
 
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
+  addCommentSubmit.addEventListener('click', function(event) {
+    event.preventDefault();
+    var commentContent = document.getElementById('comment_content').value;
+    var params = {
+      "content" : commentContent,
+      "websiteId" : WEBSITE_ID
+    }
+    addComment(params);
   });
 });
+
+
